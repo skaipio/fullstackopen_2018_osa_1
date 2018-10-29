79d65f21440a196d0e3550212b0041ea57599483
Senni Kaipio <senni.kaipio@protonmail.com>
2018-10-29 15:18:52 +0200

    Tehtävä 1.13

--- src/index.js
+++ src/index.js
@@ -6,19 +6,36 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: {}
     }
   }
 
   render() {
-    const nextAnecdote = () =>
-      this.setState({
-        selected: Math.floor(Math.random() * anecdotes.length)
-      })
+    const nextAnecdote = () => this.setState(prevState => {
+      const indices = [...Array(anecdotes.length).keys()]
+      indices.splice(prevState.selected, 1)
+      const nextSelected = indices[Math.floor((Math.random() * indices.length))]
+      return {
+        selected: nextSelected
+      }
+    })
+
+    const vote = () => this.setState(prevState => {
+      const votes = {...prevState.votes}
+      const selected = prevState.selected
+      votes[selected] = (votes[selected] || 0) + 1
+      return { votes }
+    })
+
+    const getVoteCount = () =>
+      this.state.votes[this.state.selected] || 0
 
     return (
       <div>
         <p>{this.props.anecdotes[this.state.selected]}</p>
+        <p>has {getVoteCount()} votes</p>
+        <button onClick={vote}>vote</button>
         <button onClick={nextAnecdote}>next anecdote</button>
       </div>
     )
