38917ccda71f42c33a773428b7f996e89dc7cbc2
Senni Kaipio <senni.kaipio@protonmail.com>
2018-10-29 14:42:23 +0200

    Tehtävä 1.9

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ const Statistic = ({arvosana, maara}) => (
   <p>{arvosana} {maara}</p>
 )
 
-const Statistics = ({hyva = 0, neutraali = 0, huono = 0}) => {
+const Statistics = ({hyva, neutraali, huono}) => {
+  if (!hyva && !neutraali && !huono) {
+    return (
+      <div>
+        <Header name="statistiikka" />
+        <p>ei yhtään palautetta annettu</p>
+      </div>
+    )
+  }
   const yhteensa = hyva + neutraali + huono
   const keskiarvo = yhteensa !== 0 ? (hyva - huono) / yhteensa : 0
   const positiivisiaProsentti = yhteensa !== 0 ? hyva / yhteensa : 0
